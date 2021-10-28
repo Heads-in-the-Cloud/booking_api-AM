7747e304f727eb8d40c731cfbf16c93e53a308ff
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2021-10-28 15:53:12 -0700

    added validation

--- src/main/java/com/ss/training/utopia/dto/BookingDto.java
+++ src/main/java/com/ss/training/utopia/dto/BookingDto.java
@@ -4,16 +4,36 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class BookingDto {
+public class BookingCreateDto {
     private Integer id;
+
+    @NotBlank
+    @Min(0)
+    @Max(1)
     private Integer isActive;
+
+    @NotBlank
+    @Size(min=10, max=10)
     private String confirmationCode;
+
+    @NotBlank
     private Integer flightId;
+
+    @NotBlank
     private Integer userId;
+
+    @NotBlank
     private Integer agentId;
+
+    @NotBlank
+    @Email
     private String guestEmail;
+
+    @NotBlank
     private String guestPhone;
 }
